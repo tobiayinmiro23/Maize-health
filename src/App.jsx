7230facddce77e5736beef3be04854b5e2a1ddbc
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-08-16 14:59:35 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from './firebase';
+
+function App() {
+  const storage = getStorage();
+  const [file, setfile] = useState('')
+  const [loading, setloading] = useState(false)
+  const [img_url, setimg_url] = useState(null)
+  const [pestData, setpestData] = useState([])
+  const [dataReady, setdataReady] = useState(false)
+
+  console.log(file)
+  //function to analyzes image for pests
+  const getPest = (img) => {
+    fetch(`https://serverless.roboflow.com/dr.mangosteen/1?api_key=ejpzvlfUff7irWmzswU5&image=${img}`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        setloading(false)
+        setpestData(response)
+        setdataReady(true)
+      })
+      .catch(error => {
+        console.log(error);
+        setloading(false)
+        alert(error.message);
+      });
+  }
+  //uploads image to firebase get the image link and analyze it fiir pests
+  const upload = () => {
+    setpestData([])
+    setdataReady(false)
+    setloading(true)
+    const storageRef = ref(storage, `${Date.now()}${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file)
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(progress)
+      },
+      (error) => {
+        setloading(false)
+        alert(error?.message || 'an error occured,try again later')
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log(downloadURL)
+          getPest(downloadURL)
+        }).catch(error => {
+          setloading(false)
+          alert(error?.message || 'an error occured,try again later')
+        })
+      }
+    );
+  }
+  // displays the selected image
+  const getImgUrl = (e) => {
+    setfile(e.target.files[0])
+    const data = new FileReader()
+    data.addEventListener('load', () => {
+      setimg_url(data.result)
+    })
+    data.readAsDataURL(e.target.files[0])
+  }
+
+  return (
+    <>
+      <h1>Maize Pesticide Recommender</h1>
+      <div className='holder'>
+        <div className='img'><img src={img_url} alt="maize image" /></div>
+        <label className='' >
+          <h3>select an image</h3>
+        </label>
+        <div >
+          <input type="file" name="upload" accept="image/*" className='upload' onInput={getImgUrl
+          } id="" />
+          {file != '' && <button disabled={loading} onClick={upload}>{loading ? 'Loading...' : 'Scan image'}</button>
+          }
+        </div>
+        {dataReady && <Card pestData={pestData} />}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+
+const Card = ({ pestData }) => {
+    const [recommendedPesticide, setrecommendedPesticide] = useState([])
+    const [detectedPest, setdetectedPest] = useState('')
+    const [healthy, sethealthy] = useState(false)
+    const [message, setmessage] = useState('')
+    let blight = ["Proline", "Sphere Max", "Quilt", "Priaxor", "Dithane", "Prothioconazole", "Tebuconazole", "Manzate", "Azoxystrobin", "Pyraclostrobin"];
+    let grey_leaf_spot = ["Sercadis", "Solatenol", "Amistar", "Priaxor", "Echo", "Prothioconazole", "Difenoconazole", "Tebuconazole", "Trifloxystrobin", "Azoxystrobin"];
+    let common_rust = ["Folicur", "Tebuzol", "Orius", "Tilt", "Bumper", "Score", "Divident", "Rally", "Amistar", "Heritage", "Headline", "Flint", "Opera", "Tebuconazole", "Propiconazole", "Difenoconazole"];
+
+    useEffect(() => {
+        if (pestData?.predicted_classes.length === 0) setmessage('No pest detected, or pest not known by the model')
+        else if (pestData?.predicted_classes[0] === 'Blight') {
+            setdetectedPest('Blight')
+            setrecommendedPesticide(blight)
+        }
+        else if (pestData?.predicted_classes[0] === 'Common_Rust') {
+            setdetectedPest('Common_Rust')
+            setrecommendedPesticide(common_rust)
+        }
+        else if (pestData?.predicted_classes[0] === 'Gray_Leaf_Spot') {
+            setdetectedPest('Gray_Leaf_Spot')
+            setrecommendedPesticide(grey_leaf_spot)
+        }
+        else if (pestData?.predicted_classes[0] === 'Healthy') sethealthy(true)
+        else setmessage('an error occured, please try again later')
+
+    }, [])
+    console.log(detectedPest)
+    return (
+        <div className='card'>
+            {
+                (detectedPest !== '' || undefined) &&
+                <div className='card_header'>
+                    <h2>Detected Pest: </h2>
+                    <i className='pest_name'>{detectedPest}</i>
+                </div>
+            }
+            <div>
+                {
+                    recommendedPesticide.length !== 0 &&
+                    <div>
+                        <h2>Recommended Pesticides</h2>
+                        {
+                            recommendedPesticide?.map(item => {
+                                return <i key={item}>{item}</i>
+                            })
+                        }
+                    </div>
+                }
+            </div>
+            <i>{message}</i>
+            {healthy && <i>No Pest detected maize is healthy</i>}
+        </div>
+    )
+}
+
+export default Card
